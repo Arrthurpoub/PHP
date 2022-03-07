4941d470b54bb748a73ca2d862a128fa749b1cc2
Arthur <Godbillot@MSI>
2022-03-07 20:27:54 +0100

    PHP

---
+++ PHP-HTML.php
@@ -0,0 +1,18 @@
+<html>
+<head>
+    <title> 13's multiplication table  </title>
+</head>
+<body>
+
+<!--php html-->
+<?php
+for($i = 0; $i < 13; $i++)
+{
+    echo "13*". $i. "=" . 13 * $i ."<br>";
+
+}
+?>
+
+</body>
+</html>
+
